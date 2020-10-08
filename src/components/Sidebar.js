8dd793a0d1d86cc22f7cab68ac6f695d04de92dd
nsc_luis <nsc_luis@nscco.com.mx>
2020-10-08 01:29:04 -0500

    primer commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,175 @@
+import React from 'react';
+
+class Sidebar extends React.Component { 
+    render() {
+        return(
+            <div>
+                {/* Sidebar */}
+                <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                    {/* Sidebar - Brand */}
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/Index">
+                        <div className="sidebar-brand-icon">
+                            <i className="fas fa-building"></i>
+                        </div>
+                        <div className="sidebar-brand-text mx-3">IECE</div>
+                    </a>
+
+                    {/* Divider */}
+                    <hr className="sidebar-divider" />
+
+                    {/* Heading */}
+                    <div className="sidebar-heading">
+                        Sector
+                    </div>
+
+                    {/* Nav Item - Sector */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="/Sector/Index">
+                            <i className="fas fa-fw fa-place-of-worship"></i>
+                            <span>Datos generales</span>
+                        </a>
+                    </li>
+
+                    {/* Nav Item - Personal General */}
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" href="/Personal/Index">
+                            <i className="fas fa-fw fa-address-book"></i>
+                            <span>Personal general</span>
+                        </a>
+                    </li>
+
+                    {/* Nav Item - Personal bautizado
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" href="/PersonalBautizado/Index">
+                            <i className="fas fa-fw fa-id-card"></i>
+                            <span>Personal bautizado</span>
+                        </a>
+                    </li> */}
+                    {/* Nav Item - Personal no bautizado
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" href="/PersonalNoBautizado/Index">
+                            <i className="fas fa-fw fa-users"></i>
+                            <span>Personal no bautizado</span>
+                        </a>
+                    </li> */}
+                    {/* Divider */}
+                    <hr className="sidebar-divider" />
+
+                    {/* Heading */}
+                    <div className="sidebar-heading">
+                        Movimientos de personal
+                    </div>
+
+                    {/* Nav Item - Altas Collapse Menu */}
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseMPAltas" aria-expanded="true" aria-controls="collapsePages">
+                            <i className="fas fa-fw fa-user-check"></i>
+                            <span>Alta de personal</span>
+                        </a>
+                        <div id="collapseMPAltas" className="collapse" aria-labelledby="headingMPAltas" data-parent="#accordionSidebar">
+                            <div className="bg-white py-2 collapse-inner rounded">
+                                <h6 className="collapse-header">Personal bautizado:</h6>
+                                <a className="collapse-item" href="#">Bautismo</a>
+                                <a className="collapse-item" href="#">Cambio de domicilio</a>
+                                <a className="collapse-item" href="#">Restitucion</a>
+                                <h6 className="collapse-header">Personal no bautizado:</h6>
+                                <a className="collapse-item" href="#">Nuevo ingreso</a>
+                                <a className="collapse-item" href="#">Cambio de domicilio</a>
+                                <a className="collapse-item" href="#">Reactivacion</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    {/* Nav Item - Bajas Collapse Menu */}
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsempbajas" aria-expanded="true" aria-controls="collapsepages">
+                            <i className="fas fa-fw fa-user-times"></i>
+                            <span>Baja de personal</span>
+                        </a>
+                        <div id="collapseMPBajas" className="collapse" aria-labelledby="headingMPBajas" data-parent="#accordionSidebar">
+                            <div className="bg-white py-2 collapse-inner rounded">
+                                <h6 className="collapse-header">Personal bautizado:</h6>
+                                <a className="collapse-item" href="#">Cambio de domicilio</a>
+                                <a className="collapse-item" href="#">Defuncion</a>
+                                <a className="collapse-item" href="#">Excomunion</a>
+                                <h6 className="collapse-header">Personal no bautizado:</h6>
+                                <a className="collapse-item" href="#">Cambio de domicilio</a>
+                                <a className="collapse-item" href="#">Alejamiento</a>
+                                <a className="collapse-item" href="#">Defuncion</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    {/* Nav Item - Hogares */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="fas fa-fw fa-home"></i>
+                            <span>Hogares</span>
+                        </a>
+                    </li>
+
+                    {/* Divider */}
+                    <hr className="sidebar-divider" />
+
+                    {/* Heading */}
+                    <div className="sidebar-heading">
+                        Movimientos estadisticos
+                    </div>
+
+                    {/* Nav Item - Matrimonios */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="fas fa-fw fa-user-friends"></i>
+                            <span>Matrimonios</span>
+                        </a>
+                    </li>
+
+                    {/* Nav Item - Presentaciones */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="fas fa-fw fa-baby"></i>
+                            <span>Presentaciones</span>
+                        </a>
+                    </li>
+
+                    {/* Divider */}
+                    <hr className="sidebar-divider" />
+
+                    {/* Heading */}
+                    <div className="sidebar-heading">
+                        Seccion de reportes
+                    </div>
+
+                    {/* Nav Item - Reportes Collapse Menu */}
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseReportes" aria-expanded="true" aria-controls="collapsePages">
+                            <i className="fas fa-fw fa-clipboard-list"></i>
+                            <span>Reportes</span>
+                        </a>
+                        <div id="collapseReportes" className="collapse" aria-labelledby="headingReportes" data-parent="#accordionSidebar">
+                            <div className="bg-white py-2 collapse-inner rounded">
+                                <h6 className="collapse-header">Seleccione un reporte:</h6>
+                                <a className="collapse-item" href="#">Membresia general</a>
+                                <a className="collapse-item" href="#">Membresia bautizada</a>
+                                <a className="collapse-item" href="#">Lista de hogares</a>
+                                <a className="collapse-item" href="#">Informe pastoral</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    {/* Divider */}
+                    <hr className="sidebar-divider d-none d-md-block" />
+
+                    {/* Sidebar Toggler (Sidebar) */}
+                    <div className="text-center d-none d-md-inline">
+                        <button className="rounded-circle border-0" id="sidebarToggle"></button>
+                    </div>
+
+                </ul>
+                {/* End of Sidebar */}
+            </div>
+        );
+    }
+}
+export default Sidebar;
---
+++ src/components/Topbar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+class Topbar extends React.Component{
+
+    render(){
+        return(
+            <div>
+                {/* Topbar */}
+                <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    {/* Sidebar Toggle (Topbar) */}
+                    <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+                        <i className="fa fa-bars"></i>
+                    </button>
+
+                    {/* Topbar Navbar */}
+                    <ul className="navbar-nav ml-auto">
+
+                        <div className="topbar-divider d-none d-sm-block"></div>
+
+                        {/* Nav Item - User Information */}
+                        <li className="nav-item dropdown no-arrow">
+                            <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span className="mr-2 d-none d-lg-inline text-gray-600 small">Nombre del usuario</span>
+                                <img className="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" />
+                            </a>
+                            {/* Dropdown - User Information */}
+                            <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a className="dropdown-item" href="#">
+                                    <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Perfil
+                                </a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Cerrar sesion
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                {/* End of Topbar */}
+                {/* Logout Modal*/}
+                <div className="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                                <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                            <div className="modal-footer">
+                                <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                <a className="btn btn-primary" href="login.html">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* End Logout Modal*/}
+                {/* Begin Page Content */}
+                <div className="container-fluid"></div>
+            </div>   
+        );
+    }
+
+}
+
+export default Topbar;
