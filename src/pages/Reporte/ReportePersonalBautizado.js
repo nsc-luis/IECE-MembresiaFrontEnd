65507a59e96eabe71a78ca3099152da2f0b10daa
vicrulum <50632377+vicrulum@users.noreply.github.com>
2022-05-09 01:22:04 -0500

    Descargar PDF ReportePersonalBautizado

--- src/pages/Reporte/ReportePersonalBautizado.js
+++ src/pages/Reporte/ReportePersonalBautizado.js
@@ -1,12 +1,17 @@
 import Layout from "../Layout";
 import helpers from "../../components/Helpers";
 import {
-    Container, Row, Col, Form, FormGroup, Input, Button,
-    FormFeedback, CardTitle, Card, CardBody, CardHeader, CardText, Label, Alert, Table
+    Container, Button,
+     CardTitle, Card, CardBody, Table, UncontrolledCollapse, Row, Col
 } from 'reactstrap';
 
-import React, { Component, useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, } from 'react';
 import TableToExcel from "@linways/table-to-excel";
+import jsPDF from 'jspdf';
+import Moment from "react-moment";
+import moment from "moment";
+import 'moment/locale/es'
+import logo from '../../assets/images/IECE_LogoOficial.jpg'
 
 
 export default function ReportePersonalBautizado(){
@@ -14,53 +19,37 @@ export default function ReportePersonalBautizado(){
     const [personas, setPersonas] = useState([])
     const dto = JSON.parse(localStorage.getItem("dto"))
     const sector = JSON.parse(localStorage.getItem("sector"))
-
-    //dataExcel
-    const dataSet = [
-        {
-            columns: [
-                "Name",
-                "Nombre(s)",
-                "Apellido Paterno",
-                "Apellido Materno",
-                "Categoria",
-                "RFC",
-                "Telefono Movil",
-                "Sector",
-            ],
-            data: personas
-        }
-    ];
-
+    console.log(dto)
+    console.log(JSON.parse(localStorage.getItem("infoSesion")))
     //Llamadas en render
     useEffect(() => {
         if(sector == null){
             helpers.authAxios.get("/Persona/GetByDistrito/" + dto)
                 .then(res => {
-                    setPersonas(res.data.filter(persona => persona.persona.per_Bautizado))
+                    setPersonas(res.data.filter(persona => persona.persona.per_Bautizado && persona.persona.per_En_Comunion))
                     console.log("OBISPO")
                     console.log(personas)
                 });
         }else{
             helpers.authAxios.get("/Persona/GetBySector/" + sector)
             .then(res => {
-                setPersonas(res.data.filter(persona => persona.persona.per_Bautizado))
+                setPersonas(res.data.filter(persona => persona.persona.per_Bautizado && persona.persona.per_En_Comunion))
                 console.log("PASTOR")
                 console.log(personas)
             });
         }
 
-        const mappedPersonas = personas.map(persona => ({
-            per_Id_Persona: persona.per_Id_Persona,
-            per_Nombre: persona.per_Nombre,
-            per_Apellido_Paterno: persona.per_Apellido_Paterno,
-            per_Apellido_Materno: persona.per_Apellido_Materno,
-            per_Categoria: persona.per_Categoria,
-            per_RFC_Sin_Homo: persona.per_RFC_Sin_Homo,
-            per_Telefono_Movil: persona.per_Telefono_Movil,
-            sec_Alias: persona.sec_Alias
-        }))
-        console.log(mappedPersonas)
+        // const mappedPersonas = personas.map(persona => ({
+        //     per_Id_Persona: persona.per_Id_Persona,
+        //     per_Nombre: persona.per_Nombre,
+        //     per_Apellido_Paterno: persona.per_Apellido_Paterno,
+        //     per_Apellido_Materno: persona.per_Apellido_Materno,
+        //     per_Categoria: persona.per_Categoria,
+        //     per_RFC_Sin_Homo: persona.per_RFC_Sin_Homo,
+        //     per_Telefono_Movil: persona.per_Telefono_Movil,
+        //     sec_Alias: persona.sec_Alias
+        // }))
+        // console.log(mappedPersonas)
     }, [personas.length])
 
     const downloadTable = () =>{
@@ -71,39 +60,256 @@ export default function ReportePersonalBautizado(){
             }
           });
     }
+
+    let totalCount = 0;
+
+    const countPersons = (type) =>{
+        let count = 0
+        personas.map(persona => {
+            if(persona.persona.per_Categoria === type){
+                count+=1 
+            }
+        })
+        totalCount += count;
+        return count
+    }
+    const reportePersonalBautizadoPDF = () =>{
+        totalCount = 0
+        // INSTANCIA NUEVO OBJETO PARA CREAR PDF
+        const doc = new jsPDF("p", "mm", "letter");
+
+        doc.addImage(logo, 'PNG', 10, 5, 70, 20);
+        doc.text("REPORTE DE PERSONAL BAUTIZADO", 85, 10);
+        doc.setFontSize(8);
+        doc.text(`DISTRITO: ${JSON.parse(localStorage.getItem("infoSesion")).dis_Alias}`, 85, 15)
+        
+        if (sector) {
+            doc.text(`SECTOR: ${JSON.parse(localStorage.getItem("infoSesion")).sec_Alias}`, 85, 20);
+            doc.text(`AL DÍA ${moment().format('LL').toUpperCase()}`, 85, 25);
+        }
+        else {
+            doc.text(`AL DÍA ${moment().format('LL').toUpperCase()}`, 85, 20);
+        }
+        doc.line(10, 32, 200, 32);
+        
+        let yAxis = 35
+        doc.setFillColor(137, 213, 203) // Codigos de color RGB (red, green, blue)
+        doc.rect(10, yAxis, 190, 4, "F");
+        doc.setFont("", "", "bold");
+        yAxis += 3;
+        doc.text("ADULTOS HOMBRES", 15, yAxis);
+        doc.text(`${countPersons("ADULTO_HOMBRE")}`, 80, yAxis);
+        yAxis += 7;
+        personas.map((persona, index) => {
+            if(persona.persona.per_Categoria === "ADULTO_HOMBRE"){
+                doc.text(`${index + 1}.- ${persona.persona.per_Nombre} ${persona.persona.per_Apellido_Paterno} ${persona.persona.per_Apellido_Materno}`, 20, yAxis);
+                yAxis+=5;
+            }
+        })
+
+        yAxis += 7;
+        doc.setFillColor(137, 213, 203) // Codigos de color RGB (red, green, blue)
+        doc.rect(10, yAxis, 190, 4, "F");
+        doc.setFont("", "", "bold");
+        yAxis += 3;
+        doc.text("ADULTOS MUJERES", 15, yAxis);
+        doc.text(`${countPersons("ADULTO_MUJER")}`, 80, yAxis);
+        yAxis += 7;
+        personas.map((persona, index) => {
+            if(persona.persona.per_Categoria === "ADULTO_MUJER"){
+                doc.text(`${index + 1}.- ${persona.persona.per_Nombre} ${persona.persona.per_Apellido_Paterno} ${persona.persona.per_Apellido_Materno}`, 20, yAxis);
+                yAxis+=5;
+            }
+        })
+
+        yAxis += 7;
+        doc.setFillColor(137, 213, 203) // Codigos de color RGB (red, green, blue)
+        doc.rect(10, yAxis, 190, 4, "F");
+        doc.setFont("", "", "bold");
+        yAxis += 3;
+        doc.text("JOVENES HOMBRES", 15, yAxis);
+        doc.text(`${countPersons("JOVEN_HOMBRE")}`, 80, yAxis);
+        yAxis += 7;
+        personas.map((persona, index) => {
+            if(persona.persona.per_Categoria === "JOVEN_HOMBRE"){
+                doc.text(`${index + 1}.- ${persona.persona.per_Nombre} ${persona.persona.per_Apellido_Paterno} ${persona.persona.per_Apellido_Materno}`, 20, yAxis);
+                yAxis+=5;
+            }
+        })
+
+        yAxis += 7;
+        doc.setFillColor(137, 213, 203) // Codigos de color RGB (red, green, blue)
+        doc.rect(10, yAxis, 190, 4, "F");
+        doc.setFont("", "", "bold");
+        yAxis += 3;
+        doc.text("JOVENES MUJERES", 15, yAxis);
+        doc.text(`${countPersons("JOVEN_MUJER")}`, 80, yAxis);
+        yAxis += 7;
+        personas.map((persona, index) => {
+            if(persona.persona.per_Categoria === "JOVEN_MUJER"){
+                doc.text(`${index + 1}.- ${persona.persona.per_Nombre} ${persona.persona.per_Apellido_Paterno} ${persona.persona.per_Apellido_Materno}`, 20, yAxis);
+                yAxis+=5;
+            }
+        })
+
+        yAxis += 2;
+        doc.rect(75, yAxis, 15, 4);
+        yAxis += 3;
+        doc.text("TOTAL DE PERSONAL BAUTIZADO:", 20, yAxis);
+        doc.text(`${totalCount}`, 80, yAxis);
+
+        yAxis += 25;
+        doc.text(`JUSTICIA Y VERDAD`, 90, yAxis);
+        yAxis += 5;
+        doc.text(`AL DÍA ${moment().format('LL').toUpperCase()}`, 85, yAxis);
+
+        yAxis += 35;
+        doc.line(30, yAxis, 90, yAxis);
+        doc.line(120, yAxis, 180, yAxis);
+        yAxis += 3;
+        doc.text("SECRETARIO", 51, yAxis);
+        doc.text("PASTOR", 145, yAxis);
+        yAxis -= 5;
+        doc.text(`${JSON.parse(localStorage.getItem("infoSesion")).pem_Nombre}`, 130, yAxis);
+
+
+        doc.save("ReportePersonalBautizado.pdf");
+    }
     return(
         <Layout>
             <Container>
-            <Button className="btn-success m-3 " onClick={() => downloadTable()}><i className="fas fa-file-excel mr-2"></i>Descargar Excel</Button>
+                <Button className="btn-success m-3 " onClick={() => downloadTable()}><i className="fas fa-file-excel mr-2"></i>Descargar Excel</Button>
+                <Button className="btn-danger m-3 " onClick={() => reportePersonalBautizadoPDF()}><i className="fas fa-file-pdf mr-2"></i>Descargar PDF</Button>
                 <Card body>
-                <CardTitle className="text-center" tag="h4">
+                    <Row>
+                        <Col lg="5">
+                            <img src={logo} width="100%"></img> 
+                        </Col>
+                        <Col lg="7">
+                            <CardTitle className="text-left" tag="h3">
+                                REPORTE DE PERSONAL BAUTIZADO
+                                <h5 className="mt-3"><strong>Distrito: </strong>{JSON.parse(localStorage.getItem("infoSesion")).dis_Alias}</h5>
+                                {sector ? <h5 className="mt-3"><strong>Sector: </strong>{JSON.parse(localStorage.getItem("infoSesion")).sec_Alias}</h5> : null}
+                            </CardTitle>
+                        </Col>
+                    </Row>
+                    <CardBody>
+                        <Button color="primary" size="lg" className="text-left mb-2" block id="adultos_hombres">Adultos hombres: {countPersons("ADULTO_HOMBRE")}</Button>
+                        <UncontrolledCollapse defaultOpen toggler="#adultos_hombres">
+                            <Card>
+                                <CardBody>
+                                    <h5>
+                                        <ol type="1">
+                                        {personas.map((persona) => {
+                                            if(persona.persona.per_Categoria === "ADULTO_HOMBRE"){
+                                                return <li key={persona.persona.per_Id_Persona}>{persona.persona.per_Nombre} {persona.persona.per_Apellido_Paterno} {persona.persona.per_Apellido_Materno}</li>
+                                            }
+                                        })}
+                                        </ol>
+                                    </h5>
+                                </CardBody>
+                            </Card>
+                        </UncontrolledCollapse>
+                        <Button color="primary" size="lg" className="text-left mb-2" block id="adultos_mujeres">Adultos mujeres: {countPersons("ADULTO_MUJER")}</Button>
+                        <UncontrolledCollapse defaultOpen toggler="#adultos_mujeres">
+                            <Card>
+                                <CardBody>
+                                    <h5>
+                                        <ol type="1">
+                                        {personas.map((persona) => {
+                                            if(persona.persona.per_Categoria === "ADULTO_MUJER"){
+                                                return <li key={persona.persona.per_Id_Persona}>{persona.persona.per_Nombre} {persona.persona.per_Apellido_Paterno} {persona.persona.per_Apellido_Materno}</li>
+                                            }
+                                        })}
+                                        </ol>
+                                    </h5>
+                                </CardBody>
+                            </Card>
+                        </UncontrolledCollapse>
+                        <Button color="primary" size="lg" className="text-left mb-2" block id="jovenes_hombres">Jovenes hombres: {countPersons("JOVEN_HOMBRE")}</Button>
+                        <UncontrolledCollapse defaultOpen toggler="#jovenes_hombres">
+                            <Card>
+                                <CardBody>
+                                    <h5>
+                                        <ol type="1">
+                                        {personas.map((persona) => {
+                                            if(persona.persona.per_Categoria === "JOVEN_HOMBRE"){
+                                                return <li key={persona.persona.per_Id_Persona}>{persona.persona.per_Nombre} {persona.persona.per_Apellido_Paterno} {persona.persona.per_Apellido_Materno}</li>
+                                            }
+                                        })}
+                                        </ol>
+                                    </h5>
+                                </CardBody>
+                            </Card>
+                        </UncontrolledCollapse>
+                        <Button color="primary" size="lg" className="text-left mb-2" block id="jovenes_mujeres">Jovenes mujeres: {countPersons("JOVEN_MUJER")}</Button>
+                        <UncontrolledCollapse defaultOpen toggler="#jovenes_mujeres">
+                            <Card>
+                                <CardBody>
+                                    <h5>
+                                        <ol type="1">
+                                        {personas.map((persona) => {
+                                            if(persona.persona.per_Categoria === "JOVEN_MUJER"){
+                                                return <li key={persona.persona.per_Id_Persona}>{persona.persona.per_Nombre} {persona.persona.per_Apellido_Paterno} {persona.persona.per_Apellido_Materno}</li>
+                                            }
+                                        })}
+                                        </ol>
+                                    </h5>
+                                </CardBody>
+                            </Card>
+                        </UncontrolledCollapse>
+
+                        <h4 className="text-right m-4">Total de personal bautizado: <strong>{totalCount}</strong></h4>
+                        <h4 className="text-center m-4">Justicia y Verdad</h4>
+                        {sector ?
+                            <h4 className="text-center m-4">{JSON.parse(localStorage.getItem("infoSesion")).sec_Alias} a <Moment locale="es" format="LL"></Moment></h4> :
+                            <h4 className="text-center m-4">{JSON.parse(localStorage.getItem("infoSesion")).dis_Alias} a <Moment locale="es" format="LL"></Moment></h4>
+                        }
+                        <Row className="text-center mt-5">
+                            <Col>
+                                <h5 style={{height: "1.2em"}}></h5>
+                                {/* <Input className="text-center" bsSize="sm" type="text" placeholder="Escriba nombre del secretario"></Input> */}
+                                <hr color="black"></hr>
+                                <h5>Secretario</h5>
+                            </Col>
+                            <Col>
+                                <h5>{JSON.parse(localStorage.getItem("infoSesion")).pem_Nombre}</h5>
+                                <hr color="black"></hr>
+                                {sector ? <h5>Pastor</h5> : <h5>Obispo</h5>}
+                            </Col>
+                        </Row>
+                </CardBody>
+                </Card>
+
+
+                {/* TABLA */}
+                <Card hidden body>
+                <CardTitle className="text-center" tag="h3">
                     Reporte de Personal Bautizado
+                    <h5>Distrito: {JSON.parse(localStorage.getItem("infoSesion")).dis_Alias}</h5>
+                    {sector ? <h5>Sector: {JSON.parse(localStorage.getItem("infoSesion")).sec_Alias}</h5> : null}
                 </CardTitle>
                 <CardBody>
-                    <Table responsive hover id="table1" data-cols-width="10,20,20,20,20,20,20,30">
+                    <Table responsive hover id="table1" data-cols-width="10,20,20,20,20,20">
                         <thead>
                             <tr>
-                                <th data-f-bold>ID</th>
+                                <th data-f-bold>Indice</th>
                                 <th data-f-bold>Nombre(s)</th>
                                 <th data-f-bold>Apellido Paterno</th>
                                 <th data-f-bold>Apellido Materno</th>
                                 <th data-f-bold>Categoria</th>
-                                <th data-f-bold>RFC</th>
                                 <th data-f-bold>Telefono Movil</th>
-                                <th data-f-bold>Sector</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {personas.map(persona => (
+                            {personas.map((persona, index) => (
                                 <tr key={persona.persona.per_Id_Persona}>
-                                    <td>{persona.persona.per_Id_Persona}</td>
+                                    <td>{index + 1}</td>
                                     <td>{persona.persona.per_Nombre}</td>
                                     <td>{persona.persona.per_Apellido_Paterno}</td>
                                     <td>{persona.persona.per_Apellido_Materno}</td>
                                     <td>{persona.persona.per_Categoria}</td>
-                                    <td>{persona.persona.per_RFC_Sin_Homo}</td>
                                     <td>{persona.persona.per_Telefono_Movil}</td>
-                                    <td>{persona.persona.sec_Alias}</td>
                                 </tr>
                             ))}
                         </tbody>
