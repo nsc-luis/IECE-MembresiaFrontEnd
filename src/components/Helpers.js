0aaeb4240fb0d823b63cd8e50dea1dd178aef6cb
nsc_luis <nsc_luis@nscco.com.mx>
2022-06-22 11:55:54 -0500

    actualizacion de frontend

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -41,9 +41,9 @@ const helpers = {
     },
 
     // URLs PARA PRUEBA
-    url_api: "http://" + window.location.hostname + ":59239/api",
+    //url_api: "http://" + window.location.hostname + ":59239/api",
     // url_api: "http://iece-tpr.ddns.net/webapi/api",
-    // url_api : "http://" + window.location.hostname + "/webapi/api",
+    url_api : "http://" + window.location.hostname + "/webapi/api",
 
     // METODO PARA VALIDAR CAMPOS
     validaFormatos: function (formato, campo) {
